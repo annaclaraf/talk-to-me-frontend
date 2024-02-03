f69c7a9c49c25a008309376fd8882d48db0d04bc
Anna Clara <anisferraz@gmail.com>
2024-02-02 22:03:04 -0300

    feat: join room component

---
+++ src/components/Join.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useRef } from 'react';
+import { Button } from './Button';
+import { Input } from './Input';
+
+export function JoinRoom() {
+  const name = useRef<HTMLInputElement>(null);
+  const id = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <Input placeholder="Seu nome" type="text" ref={name} />
+      <Input placeholder="ID da reunião" type="text" ref={id} />
+
+      <Button title="Entrar" type="button" />
+    </>
+  );
+}
