55af5112ad39700d4570551ec5e6502df9350f7a
Anna Clara <anisferraz@gmail.com>
2024-02-10 21:47:18 -0300

    feat: join a room

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useRef } from 'react';
+import { useRef, FormEvent } from 'react';
 import { Button } from './Button';
 import { Input } from './Input';
 
@@ -7,12 +7,22 @@ export function JoinRoom() {
   const name = useRef<HTMLInputElement>(null);
   const id = useRef<HTMLInputElement>(null);
 
+  function handleJoinRoom(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (name.current && name.current.value !== '' && id.current && id.current.value !== '') {
+      sessionStorage.setItem("username", name.current.value);
+      const roomId = id.current.value;
+      window.location.href = `/room/${roomId}`;
+    }
+  }
+
   return (
-    <>
+    <form onSubmit={(e) => handleJoinRoom(e)} className="space-y-8">
       <Input placeholder="Seu nome" type="text" ref={name} />
       <Input placeholder="ID da reunião" type="text" ref={id} />
 
-      <Button title="Entrar" type="button" />
-    </>
+      <Button title="Entrar" type="submit" />
+    </form>
   );
 }
