950dae3a081d3cf56b43d9f6d75c1ffe15ecd9f5
Anna Clara <anisferraz@gmail.com>
2024-02-11 19:59:56 -0300

    feat: add video streaming functionality

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,22 +1,34 @@
 'use client'
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { Chat } from "@/components/Chat";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+
+import { SocketContext } from "@/contexts/SocketContext";
 
 interface IAnswer {
   sender: string;
   description: RTCSessionDescriptionInit;
 }
 
-import { SocketContext } from "@/contexts/SocketContext";
+interface ICandidate {
+  sender: string;
+  candidate: RTCIceCandidate;
+}
+
+interface IDataStream {
+  id: string;
+  stream: MediaStream;
+}
 
 export default function Room({ params }: { params: { id: string } }) {
   const { socket } = useContext(SocketContext);
 
   const userCam = useRef<HTMLVideoElement>(null);
   const peerConnections = useRef<Record<string, RTCPeerConnection>>({});
+  const [remoteStreams, setRemoteStreams] = useState<IDataStream[]>([]);
+  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
 
   useEffect(() => {
     socket?.on("connect", async () => {
@@ -24,7 +36,7 @@ export default function Room({ params }: { params: { id: string } }) {
         roomId: params.id,
         socketId: socket.id
       });
-      await initCamera();
+      await initLocalCamera();
     });
 
     socket?.on("new user", data => {
@@ -43,6 +55,10 @@ export default function Room({ params }: { params: { id: string } }) {
     socket?.on("sdp", data => {
       handleAnswer(data);
     });
+
+    socket?.on("ice candidates", data => {
+      handleIceCandidates(data);
+    });
   }, [socket]);
 
   async function createPeerConnection(socketId: string, createOffer?: boolean) {
@@ -50,9 +66,20 @@ export default function Room({ params }: { params: { id: string } }) {
     const peer = new RTCPeerConnection(config);
     peerConnections.current[socketId] = peer;
 
-    if (createOffer) {
-      const peerConnection = peerConnections.current[socketId];
+    const peerConnection = peerConnections.current[socketId];
 
+    if (localStream) {
+      localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream);
+      });
+    } else {
+      const video = await initRemoteCamera();
+      video.getTracks().forEach(track => {
+        peerConnection.addTrack(track, video);
+      });
+    }
+
+    if (createOffer) {
       const offer = await peerConnection.createOffer();
       await peerConnection.setLocalDescription(offer);
 
@@ -62,6 +89,32 @@ export default function Room({ params }: { params: { id: string } }) {
         description: peerConnection.localDescription,
       });
     }
+
+    peerConnection.ontrack = (event) => {
+      const remoteStream = event.streams[0];
+
+      const dataStream: IDataStream = {
+        id: socketId,
+        stream: remoteStream
+      };
+
+      setRemoteStreams((prevState: IDataStream[]) => {
+        if (!prevState.some((stream) => stream.id === socketId)) {
+          return [...prevState, dataStream];
+        }
+        return prevState;
+      });
+    };
+
+    peer.onicecandidate = event => {
+      if (event.candidate) {
+        socket?.emit("ice candidates", {
+          to: socketId,
+          sender: socket.id,
+          candidate: event.candidate,
+        });
+      }
+    };
   }
 
   async function handleAnswer(data: IAnswer){
@@ -85,12 +138,28 @@ export default function Room({ params }: { params: { id: string } }) {
     }
   };
 
-  async function initCamera() {
+  async function handleIceCandidates(data: ICandidate) {
+    const peerConnection = peerConnections.current[data.sender];
+    if (data.candidate) {
+      await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+    }
+  }
+
+  async function initLocalCamera() {
     const video = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: { noiseSuppression: true, echoCancellation: true },
     });
+    setLocalStream(video);
     if (userCam.current) userCam.current.srcObject = video;
+  }
+
+  async function initRemoteCamera() {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: { noiseSuppression: true, echoCancellation: true },
+    });
+    return video;
   }
 
   return (
@@ -109,20 +178,21 @@ export default function Room({ params }: { params: { id: string } }) {
               <span className="absolute bottom-3">Anna Clara</span>
             </div>
 
-            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
-              <video className="h-full w-full" />
-              <span className="absolute bottom-3">Anna Clara</span>
-            </div>
-
-            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
-              <video className="h-full w-full" />
-              <span className="absolute bottom-3">Anna Clara</span>
-            </div>
-            
-            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
-              <video className="h-full w-full" />
-              <span className="absolute bottom-3">Anna Clara</span>
-            </div>
+            {remoteStreams.map((stream, index) => {
+              return (
+                <div className="bg-gray-950 w-full rounded-md h-full p-2 relative" key={index}>
+                  <video
+                    className="h-full w-full"
+                    ref={(video) => {
+                      if (video && video.srcObject !== stream.stream)
+                      video.srcObject = stream.stream;
+                    }}
+                    autoPlay
+                  />
+                  <span className="absolute bottom-3">Clara</span>
+                </div>
+              );
+            })}
           </div>
         </div>
         <Chat roomId={params.id} />
