faa713871574f5d6dc9fb2eb5921558f73ff8d3a
Anna Clara <anisferraz@gmail.com>
2024-02-10 21:48:06 -0300

    feat: enable user camera

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 import { Chat } from "@/components/Chat";
 import Footer from "@/components/Footer";
@@ -10,13 +10,25 @@ import { SocketContext } from "@/contexts/SocketContext";
 export default function Room({ params }: { params: { id: string } }) {
   const { socket } = useContext(SocketContext);
 
+  const userCam = useRef<HTMLVideoElement>(null);
+
   useEffect(() => {
-    socket?.on("connect", () => console.log("connected"));
-    socket?.emit("subscribe", { 
-      roomId: params.id,
-      socketId: socket.id
+    socket?.on("connect", async () => {
+      socket?.emit("subscribe", { 
+        roomId: params.id,
+        socketId: socket.id
+      });
+      await initCamera();
     });
   }, [socket]);
+
+  async function initCamera() {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: { noiseSuppression: true, echoCancellation: true },
+    });
+    if (userCam.current) userCam.current.srcObject = video;
+  }
 
   return (
     <div className="h-screen">
@@ -25,7 +37,12 @@ export default function Room({ params }: { params: { id: string } }) {
         <div className="md:w-[85%] w-full m-3">
           <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
-              <video className="h-full w-full" />
+              <video
+                className="h-full w-full"
+                ref={userCam}
+                autoPlay
+                playsInline
+              />
               <span className="absolute bottom-3">Anna Clara</span>
             </div>
 
